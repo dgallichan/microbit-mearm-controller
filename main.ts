621b32fc8fd0f4ac1bab09fc3af14bcdb25e6990
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-17 14:50:12 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,16 +7,21 @@ basic.showLeds(`
     . . # . .
     `)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        radio.sendValue("goLeft", -1 * input.rotation(Rotation.Pitch))
-        radio.sendValue("goRight", input.rotation(Rotation.Roll))
+    if (input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
+        radio.sendValue("goLeft", 0)
+        radio.sendValue("goRight", 0)
         radio.sendValue("goBase", 0)
-        radio.sendValue("goGrip", 0)
+        radio.sendValue("goGrip", -1 * input.rotation(Rotation.Pitch))
     } else if (input.buttonIsPressed(Button.B)) {
         radio.sendValue("goLeft", 0)
         radio.sendValue("goRight", 0)
         radio.sendValue("goBase", input.rotation(Rotation.Roll))
-        radio.sendValue("goGrip", -1 * input.rotation(Rotation.Pitch))
+        radio.sendValue("goGrip", 0)
+    } else if (input.buttonIsPressed(Button.A)) {
+        radio.sendValue("goLeft", -1 * input.rotation(Rotation.Pitch))
+        radio.sendValue("goRight", input.rotation(Rotation.Roll))
+        radio.sendValue("goBase", 0)
+        radio.sendValue("goGrip", 0)
     } else {
         radio.sendValue("goRight", 0)
         radio.sendValue("goLeft", 0)
